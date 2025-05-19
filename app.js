f62ff785275b33d4f04c1ffd819841e9381762ca
PinkiDhimmar <Pinkidhimmar@gmail.com>
2025-05-19 17:46:46 +1200

    adding

--- app.js
+++ app.js
@@ -3,12 +3,22 @@ console.log("Application is starting");
 const express = require('express')
 const app = express()
 const port = 3000
- 
-app.get("/", (req, res, next) => {
-        console.log("I received a get request on the path // ")
-    res.send("<h1>Acknowledged</h1>")
-})
- 
+
+app.use('/public', express.static('public'));
+
+app.set('view engine','ejs');
+app.get('/', function (req, res){
+ res.render("home");
+ });
+
+ app.get('/auckland', function (req, res){
+ res.render("auckland");
+ });
+
+ app.get('/beaches', function (req, res){
+ res.render("beaches");
+ });
+
 // app.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
